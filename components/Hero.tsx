3b54f37fd176f2e1f8b1a37cdec42c124c9708a1
George <georgeenueshike@gmail.com>
2025-01-16 21:08:33 +0100

    adjustment to the hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -52,7 +52,7 @@ const Hero = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="I Take Business Ideas and Turn Them Into Production Ready Applications"
+            words="From Business Ideas to Production Ready Applications"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
