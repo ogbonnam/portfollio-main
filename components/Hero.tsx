0e570c46fb179117da741ec6cae969337c50f066
George <georgeenueshike@gmail.com>
2025-01-16 21:51:38 +0100

    adjustment to the hero section2

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Martins, a Software (DEVOPS) Engineer in Nigeria.
+            Hi! I&apos;m Martins, a Software (DevOps) Engineer in Nigeria.
           </p>
 
           <a href="#about">
